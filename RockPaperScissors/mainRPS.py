457cceee67ac43107c6c3775c7f8aee3747149db
alberto <alberto.ortizort@alumnos.upm.es>
2025-07-15 18:08:50 +0200

    Rock Paper Scissors done

--- RockPaperScissors/mainRPS.py
+++ RockPaperScissors/mainRPS.py
@@ -17,12 +17,22 @@ def check_win(player, computer):
     print(f"Player chose: {player} and Computer chose: {computer}")
     if(player == computer):
         return "It's a tie!"
-    elif (player == "rock" and computer == "scissors"):
-        return "Rock smashes scissors! You win!"
-    elif (player == "paper" and computer == "rock"):
-        return "Paper covers rock! You win!"
-    elif (player == "scissors" and computer == "paper"):
-        return "Scissors cut paper! You win!"
-    else:
-        return "You lose! Better luck next time."
+    elif (player == "scissors"):
+        if (computer == "rock"):
+            return "Rock smashes scissors! You lose!"
+        else:
+            return "Scissors cut paper! You win!"
+    elif (player == "rock"):
+        if (computer == "paper"):
+            return "Paper covers rock! You lose!"
+        else:
+            return "Rock smashes scissors! You win!"
+    elif (player == "paper"):
+        if (computer == "scissors"):
+            return "Scissors cut paper! You lose!"
+        else:
+            return "Paper covers rock! You win!"
 
+choices = get_choices()
+result = check_win(choices["player"], choices["computer"])
+print(result)
